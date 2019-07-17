fda6f30a3258a2adda8682acff4f0838296b4bba
Camilo Agudelo <camilo@Camilos-MacBook-Pro-2.local>
2019-07-17 00:54:45 -0400

    Change of design Home and About page updated

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,24 +1,57 @@
-$("#startLink").click(function() {
+$(window).scroll(function () {
+    
+
+    if ($(document).scrollTop() > 60) {
+        $('nav').addClass('shrink')
+    }
+    else {
+        $('nav').removeClass('shrink')
+    }
+
+    if ($(document).scrollTop() > 450) {
+        $('nav').addClass('shrinkBlue')
+    }
+    else {
+        $('nav').removeClass('shrinkBlue')
+    }    
+})
+
+$("#startLink").click(function (e) {
+    e.preventDefault()
+    // $('.navLink').addClass('te')
+    // $(e.target).addClass('dark-primary-text')
+    // $(e.target).attr("style", {color: '#1976D2 !important'})
+    // #1976D2
     $('html,body').animate({
-        scrollTop: $("#start").offset().top},
+        scrollTop: $("#start").offset().top
+    },
         'slow');
 });
 
-$("#sumaryLink").click(function() {
+
+$("#sumaryLink").click(function (e) {
+    e.preventDefault()    
+    // $('.navLink').addClass('text-primary-color')
+    // $(e.target).addClass('dark-primary-text')
     $('html,body').animate({
-        scrollTop: $("#about").offset().top},
+        scrollTop: $("#about").offset().top
+    },
         'slow');
 });
 
-$("#skillsLink").click(function() {
+$("#skillsLink").click(function (e) {
+    e.preventDefault()    
     $('html,body').animate({
-        scrollTop: $("#skills").offset().top},
+        scrollTop: $("#skills").offset().top
+    },
         'slow');
 });
 
-$("#portfolioLink").click(function() {
+$("#portfolioLink").click(function (e) {
+    e.preventDefault()
     $('html,body').animate({
-        scrollTop: $("#portfolio").offset().top},
+        scrollTop: $("#portfolio").offset().top
+    },
         'slow');
 });
 
